67d33de0fb178fa52580f7c60c9f2c86435540f2
ameeracadam <ameera.chumma.adam@gmail.com>
2025-04-23 15:07:18 -0400

    included delta report

---
+++ variables.py
@@ -0,0 +1,62 @@
+state_territory = {
+    # States
+    "Alabama": "AL", "Alaska": "AK", "Arizona": "AZ", "Arkansas": "AR",
+    "California": "CA", "Colorado": "CO", "Connecticut": "CT", "Delaware": "DE",
+    "Florida": "FL", "Georgia": "GA", "Hawaii": "HI", "Idaho": "ID",
+    "Illinois": "IL", "Indiana": "IN", "Iowa": "IA", "Kansas": "KS",
+    "Kentucky": "KY", "Louisiana": "LA", "Maine": "ME", "Maryland": "MD",
+    "Massachusetts": "MA", "Michigan": "MI", "Minnesota": "MN", "Mississippi": "MS",
+    "Missouri": "MO", "Montana": "MT", "Nebraska": "NE", "Nevada": "NV",
+    "New Hampshire": "NH", "New Jersey": "NJ", "New Mexico": "NM", "New York": "NY",
+    "North Carolina": "NC", "North Dakota": "ND", "Ohio": "OH", "Oklahoma": "OK",
+    "Oregon": "OR", "Pennsylvania": "PA", "Rhode Island": "RI", "South Carolina": "SC",
+    "South Dakota": "SD", "Tennessee": "TN", "Texas": "TX", "Utah": "UT",
+    "Vermont": "VT", "Virginia": "VA", "Washington": "WA", "West Virginia": "WV",
+    "Wisconsin": "WI", "Wyoming": "WY",
+
+    # Territories
+    "American Samoa": "AS", "Guam": "GU", "Commonwealth of the Northern Mariana Islands": "MP",
+    "Puerto Rico": "PR", "U.S. Virgin Islands": "VI",
+
+    # Federal District
+    "District of Columbia": "DC"
+}
+
+state_to_region = {
+    # Region 1
+    "Connecticut": 1, "Maine": 1, "Massachusetts": 1, "New Hampshire": 1,
+    "Rhode Island": 1, "Vermont": 1,
+
+    # Region 2
+    "New Jersey": 2, "New York": 2, "Puerto Rico": 2, "U.S. Virgin Islands": 2,
+
+    # Region 3
+    "Delaware": 3, "District of Columbia": 3, "Maryland": 3, "Pennsylvania": 3,
+    "Virginia": 3, "West Virginia": 3,
+
+    # Region 4
+    "Alabama": 4, "Florida": 4, "Georgia": 4, "Kentucky": 4,
+    "Mississippi": 4, "North Carolina": 4, "South Carolina": 4, "Tennessee": 4,
+
+    # Region 5
+    "Illinois": 5, "Indiana": 5, "Michigan": 5, "Minnesota": 5,
+    "Ohio": 5, "Wisconsin": 5,
+
+    # Region 6
+    "Arkansas": 6, "Louisiana": 6, "New Mexico": 6, "Oklahoma": 6, "Texas": 6,
+
+    # Region 7
+    "Iowa": 7, "Kansas": 7, "Missouri": 7, "Nebraska": 7,
+
+    # Region 8
+    "Colorado": 8, "Montana": 8, "North Dakota": 8,
+    "South Dakota": 8, "Utah": 8, "Wyoming": 8,
+
+    # Region 9
+    "Arizona": 9, "California": 9, "Hawaii": 9, "Nevada": 9,
+    "American Samoa": 9, "Guam": 9, "Commonwealth of the Northern Mariana Islands": 9,
+
+    # Region 10
+    "Alaska": 10, "Idaho": 10, "Oregon": 10, "Washington": 10
+}
+
